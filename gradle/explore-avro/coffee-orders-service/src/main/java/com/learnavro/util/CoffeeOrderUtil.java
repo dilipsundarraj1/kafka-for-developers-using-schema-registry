ca2d4d11a0cf16d72daddcf74e2c56cfd3e67217
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2022-06-18 16:56:58 -0500

    added the coffee orders service

---
+++ gradle/explore-avro/coffee-orders-service/src/main/java/com/learnavro/util/CoffeeOrderUtil.java
@@ -0,0 +1,78 @@
+package com.learnavro.util;
+
+import com.learnavro.domain.generated.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
+
+public class CoffeeOrderUtil {
+
+    public static CoffeeOrder buildNewCoffeeOrder(){
+        var orderId=   new OrderId().newBuilder()
+                .setId(randomId())
+                .build();
+
+        return CoffeeOrder.newBuilder()
+                .setId(orderId)
+                .setName("Dilip Sundarraj")
+                //.setFullName("Dilip Sundarraj")
+                .setStore(generateStore())
+                .setOrderLineItems(generateOrderLineItems())
+                .setOrderedTime(LocalDateTime.now())
+               .setPickUp(PickUp.IN_STORE)
+                .build();
+
+
+    }
+
+    public static CoffeeOrderV2 buildNewCoffeeOrderV2(){
+
+        return CoffeeOrderV2.newBuilder()
+                .setId(randomId())
+                .setName("Chicago 1234")
+                .setStore(generateStore())
+                .setOrderLineItems(generateOrderLineItems())
+                .build();
+
+
+    }
+
+    private static List<OrderLineItem> generateOrderLineItems() {
+
+        var orderLineItem = OrderLineItem.newBuilder()
+                .setName("Caffe Latte")
+                .setQuantity(1)
+                .setSize(Size.MEDIUM)
+                .setCost(BigDecimal.valueOf(3.99))
+                .build();
+
+        return List.of(orderLineItem);
+
+    }
+
+    private static Store generateStore(){
+
+        return  Store.newBuilder()
+                .setId(randomId())
+                .setAddress(buildAddress())
+                .build();
+    }
+
+    private static Address buildAddress() {
+
+        return Address.newBuilder()
+                .setAddressLine1("1234 Address Line 1")
+                .setCity("Chicago")
+                .setStateProvince("IL")
+                .setZip("12345")
+                .build();
+
+    }
+
+    public static int randomId(){
+         Random random = new Random();
+         return random.nextInt(1000);
+    }
+}
