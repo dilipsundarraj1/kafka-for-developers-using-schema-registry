92a8e3eef0bb26db3c23126f0721bd48d289e565
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2022-08-05 08:30:39 -0500

    addd the avro spring boot in final branch

---
+++ gradle/avro-springboot/coffee-orders-service/src/main/java/com/learnavro/CoffeeOrdersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learnavro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrdersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeOrdersServiceApplication.class, args);
+    }
+
+}
