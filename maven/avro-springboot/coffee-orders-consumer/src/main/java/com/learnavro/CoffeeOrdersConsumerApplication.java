9a086c824cb6203291b2564420f4fd3761c083c3
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2022-08-07 19:55:49 -0500

    added the consumer app

---
+++ maven/avro-springboot/coffee-orders-consumer/src/main/java/com/learnavro/CoffeeOrdersConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.learnavro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrdersConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeOrdersConsumerApplication.class, args);
+    }
+
+}
